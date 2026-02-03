cd1e078b93a3c1ebc2e002cec08bd250713d98b9
Yetgin AKCAN <133971705+yetginakcan3@users.noreply.github.com>
2026-02-03 12:11:34 +0300

    WebUI Kısmının Hazırlanması

---
+++ ApiProjeKampi.WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _FeatureDefaultComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
